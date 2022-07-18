faaee90abbff8a1076ffca08950e43903e577250
Caroline Moreira Gonçalves <caroline.goncalves@partnerbank.com.br>
2022-07-18 11:36:10 -0300

    Alterações no projeto

---
+++ git-eclipse/src/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package dio;
+
+import dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato Gato = new Gato("Pitico","Azul",12);
+		Livros Livros = new Livros();	
+		
+		System.out.println(Gato);
+		System.out.println(Livros);
+		
+		/*int a = 20;
+		int b = 10;
+	
+		System.out.println("Hello world gareula! " + (a+b));*/
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
